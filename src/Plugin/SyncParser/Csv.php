72907f18e5bd3cf8c8caee7f1fafcef05692cf90
JaceRider <jacerider@gmail.com>
2022-07-01 12:07:19 -0500

    Support paging.

--- src/Plugin/SyncParser/Csv.php
+++ src/Plugin/SyncParser/Csv.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\sync\Plugin\SyncParser;
 
+use Drupal\aggregator\Plugin\FetcherInterface;
+use Drupal\sync\Plugin\SyncFetcherInterface;
 use Drupal\sync\Plugin\SyncParserBase;
 
 /**
@@ -27,12 +29,25 @@ class Csv extends SyncParserBase {
   /**
    * {@inheritdoc}
    */
-  protected function parse($data) {
+  protected function parse($data, SyncFetcherInterface $fetcher) {
     $use_header = $this->configuration['header'];
     $rows = array_filter(explode(PHP_EOL, $data));
     $csv = array_map(function ($row) {
       return str_getcsv($row, $this->configuration['delimiter']);
     }, $rows);
+    $page_size = $fetcher->getPageSize();
+    if ($page_size) {
+      $fetcher->setPageEnabled(TRUE);
+      if ($use_header) {
+        $header = array_shift($csv);
+      }
+      $max = $page_size * $fetcher->getPageNumber();
+      $min = $max - $page_size;
+      $csv = array_slice($csv, $min, $page_size);
+      if ($use_header) {
+        $csv = array_merge([$header], $csv);
+      }
+    }
     if ($use_header) {
       array_walk($csv, function (&$a) use ($csv) {
         if (count($csv[0]) === count($a)) {
