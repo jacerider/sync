42036038e6ee14457463adf5cf9fbe1b621f30a8
Ben Thvedt <bthvedt@augustash.com>
2025-12-03 11:57:38 -0600

    There was an error when trying to access a file and the directory didnt exist locally. This simple patch basically tries to fix it (worked for me)

--- src/Plugin/SyncResourceFileBase.php
+++ src/Plugin/SyncResourceFileBase.php
@@ -101,7 +101,16 @@ abstract class SyncResourceFileBase extends SyncResourceBase {
       $this->processItemAsNewFile($entity, $item);
     }
     else {
-      file_put_contents($entity->getFileUri(), $item['contents']);
+      $uri = $entity->getFileUri();
+
+      /** @var \Drupal\Core\File\FileSystemInterface $fs */
+      $fs = \Drupal::service('file_system');
+      $directory = $fs->dirname($uri);
+
+      // Ensure the directory exists and is writable.
+      $fs->prepareDirectory($directory, FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS);
+
+      file_put_contents($uri, $item['contents']);
       $this->processItemAsExistingFile($entity, $item);
     }
   }
@@ -149,7 +158,16 @@ abstract class SyncResourceFileBase extends SyncResourceBase {
    * Process an existing file entity.
    */
   protected function processItemAsExistingFile(FileInterface $entity, SyncDataItem $item) {
-    file_put_contents($entity->getFileUri(), $item['contents']);
+    $uri = $entity->getFileUri();
+
+    /** @var \Drupal\Core\File\FileSystemInterface $fs */
+    $fs = \Drupal::service('file_system');
+    $directory = $fs->dirname($uri);
+
+    // Ensure the directory exists and is writable.
+    $fs->prepareDirectory($directory, FileSystemInterface::CREATE_DIRECTORY | FileSystemInterface::MODIFY_PERMISSIONS);
+
+    file_put_contents($uri, $item['contents']);
     $this->renameFile($entity, $item);
   }
 
